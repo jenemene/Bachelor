1820a21a67913080e3c8f946e32179169928e6cb
jenz-thinkpad <jens.porse@gmail.com>
2026-03-09 12:50:07 +0100

    dsad

---
+++ Arbejdspakke1/Closed_N_body_non_inertial.py
@@ -0,0 +1,170 @@
+import matplotlib
+from matplotlib.pylab import norm
+import matplotlib.pyplot as plt 
+import numpy as np
+from scipy.integrate import solve_ivp
+from utils import soa as SOA
+from utils import plotting as SOAplt
+import time
+from initial_configs import initial_configs_closed as iniconf
+
+def N_body_pendulum_closed(n, tspan, state0):
+    print("Starting simulation...")
+    def ODEfun(t,state,n,link):
+        #solve_ivp passes state as np.array. It is unpacked, and then passed to ATBI as a a list of form state = [theta,beta].
+
+        #unpacking state
+        theta = state[:4*n]
+        beta = state[4*n:]
+
+        #normalizing quartenions
+        theta = SOA.normalize_quaternions(theta) 
+        
+        #calculating theta_dot based on the derrivmap function
+        theta_dot = np.zeros(len(theta))
+        for i in range(n):
+            idxq = 4*i #these indexes assume that we ONLY have spherical joints
+            idxw = 3*i
+            theta_dot[idxq:idxq+4] = SOA.derrivmap(theta[idxq:idxq+4],beta[idxw:idxw+3],"spherical")
+            
+        #Calculationg of generalized accelerations without any constraints (beta_dot_free) - this requires ATBI. 
+        tau_vec = np.zeros_like(beta) #no external torques
+
+        A_f, V_f, beta_dot_f_list, tau_bar, D, G = SOA.ATBI(state, tau_vec, n, link)
+
+        beta_dot_f = np.concatenate([b.flatten() for b in beta_dot_f_list[1:n+1]])
+
+        #Calculation of A_nd (V_nd is not needed as Q is constant) 
+
+        IR1 = SOA.get_rotation_tip_to_body_I(theta, n) #rotations to to ensure we are consistent with frames
+        IRn = SOA.spatialrotfromquat(theta[4*(n-1):4*(n-1)+4])
+        #A_nd = np.concatenate([IRn @ A_f[n],IR1 @ link.RBT.T @ A_f[1]]) # Hvis denne bruges, så tjek her om den er i rigtig rækkefølge ift. Q og udledning.
+
+        
+#Setting up Q
+        d = np.block([np.zeros((3,3)), np.eye(3)])
+        Q = np.block([d,-d])
+
+
+        #need to calculate LAMDA (the matrix thing). For that we need elements of OMEGA
+        omega_nn, omega_n1, omega_1n, omega_11= SOA.omega(theta,link,tau_bar,D,n)
+
+        # Calculating block entries JEG ER MEGET I TVIL HER, MEN VED AT LAMBDA_11 er god nok :)
+        Λ_11 = link.RBT.T @ omega_11 @ link.RBT
+        Λ_nn = omega_nn
+        Λ_n1 = omega_n1 @ link.RBT
+        Λ_1n = link.RBT.T @ omega_1n
+
+        # Rotate everything to the Inertial frame (has to be done on both sides)
+        Λ_11 = IR1 @ Λ_11 @ IR1.T
+        Λ_nn = IRn @ Λ_nn @ IRn.T
+        Λ_n1 = IR1 @ Λ_n1 @ IR1.T 
+        Λ_1n = IR1 @ Λ_1n @ IR1.T 
+
+        # Build the 12x12 block matrix
+        Λ_block = np.block([
+            [Λ_nn, Λ_n1],
+            [Λ_1n, Λ_11]
+        ])
+
+        positions = SOA.compute_pos_in_inertial_frame(state, link.l_hinge, n)
+
+        l_IO1 = positions[1]
+        l_IOn = positions[n]
+
+        IωIO = SOA.skewfromvec(IR1[:3,:3]@V_f[1][:3])
+    
+        Φ =  l_IOn - (l_IO1 + IR1[:3, :3]@link.l_hinge)
+        Φ_dot = IRn[:3, :3]@V_f[n][3:]  - (IR1[:3, :3]@V_f[1][3:] + IωIO@IR1[:3, :3]@link.l_hinge)
+        Φ_ddot =  IRn[:3, :3]@A_f[n][3:] - (IR1[:3, :3]@A_f[1][3:] + SOA.skewfromvec(IR1[:3, :3]@A_f[1][:3])@IR1[:3, :3]@link.l_hinge + IωIO@IωIO@IR1[:3,:3]@link.l_hinge)
+        
+        
+        #print(f"t={t:.2f}  |Φ| = {np.linalg.norm(Φ):.6f}")
+
+        f = SOA.baumgarte_stab(Φ, Φ_dot, Φ_ddot, 2000, 2500) # Parametrene er vi slet ikke sikker på) AYO HVORFOR FUCK HEDDER DEN F
+
+        #solving for lagrange multipliers
+        λ = -np.linalg.solve((Q@Λ_block@Q.T),f) # Dimension: 3x1
+
+
+        #calculating f_c
+        f_c_closed_loop_const =  Q.T@λ
+        f_c = [np.zeros(6,) for _ in range(n+2)]
+
+
+        f_c[n] = IRn.T @ f_c_closed_loop_const[:6]
+        f_c[1] = link.RBT @ IR1.T @ f_c_closed_loop_const[6:] 
+
+        #calculating beta_dot_delta
+        beta_dot_delta_list = SOA.beta_dot_delta(theta,tau_bar,link,n,D,f_c,G) #returns a list
+
+        beta_dot_delta = np.concatenate([b.flatten() for b in beta_dot_delta_list[1:n+1]])
+
+        beta_dot = beta_dot_delta + beta_dot_f
+
+        state_dot = np.concatenate([theta_dot, beta_dot.flatten()])
+
+        print(t)
+
+        return state_dot, V_f
+    
+    #setting up link
+    m = 20 #mass in kg
+    l_hinge = np.array([0,0,0.2])
+    link = SOA.SimpleLink(m,l_hinge)
+    link.set_hingemap("spherical")
+    
+    result, V_f = SOA.RK4_int_with_V(ODEfun, state0, tspan, n, link)
+    
+    # result = solve_ivp(
+    #     ODEfun,
+    #     t_span=(0, tspan[-1]), 
+    #     y0=state0, 
+    #     method='Radau',
+    #     t_eval=tspan,
+    #     args=(n, link),
+    #     rtol=1e-6,
+    #     atol=1e-9
+    #     )
+    
+    return result, V_f, link
+
+
+
+### SIMULATION SETTINGS ###
+n_bodies = 3
+tspan = np.arange(0, 5, 0.001)
+state0 = iniconf.N3_triangle(n_bodies)
+
+# Running and timing the simulation
+start = time.perf_counter()
+result, V_f, link = N_body_pendulum_closed(n_bodies, tspan, state0)
+end = time.perf_counter()
+
+# Extract the state matrix (Shape: [states, time_steps])
+y_out = result
+
+# Clean up any microscopic quaternion drift in the final output
+for i in range(len(tspan)):
+    # Ensure we are using your safe, non-mutating normalize_quaternions function
+    y_out[:4*n_bodies, i] = SOA.normalize_quaternions(y_out[:4*n_bodies, i])
+
+# For animation
+step = 30 
+
+t_anim = tspan[::step]
+y_anim = y_out[:, ::step]
+
+print("========================================================================================")
+print(f"Simulation time: {end - start:.4f} seconds")
+#print(f"Success: {result.success}")
+#print(f"Solver status: {result.message}")
+#print(f"Number of function evaluations: {result.nfev}")
+print("========================================================================================")
+
+SOAplt.plot_initial_state(state0, link,"closed")
+SOAplt.animation_plot(y_anim, t_anim, link, config="closed")
+
+SOAplt.check_energies(result, V_f, tspan, link, n_bodies, "closed_3")
+SOAplt.check_total_energy(result,V_f,tspan,link,n_bodies,"closed_3")
+
